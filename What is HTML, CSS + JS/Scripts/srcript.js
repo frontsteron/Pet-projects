972d1df6ddad6a624b0f9a1678deb91315e0793a
Denis Yersov <frontsteron@gmail.com>
2023-03-07 17:51:00 +0500

    Wrote a task to check a person's BMI

--- What is HTML, CSS + JS/Scripts/srcript.js
+++ What is HTML, CSS + JS/Scripts/srcript.js
@@ -1,8 +1,28 @@
 ﻿// Выводит высплывающее сообщение в браузере
-alert("Hello, World!");
+// alert("Hello, World!");
 
 // Выводит сообщение в среде Node.js
-console.log('Hello, World!');
+// console.log('Hello, World!');
 
-// Поле для ввода текста
-let age = prompt('Input your age');
+
+// Практика #1
+
+// запрос роста
+let height = parseFloat(prompt("Укажите свой рост:"));
+
+// запрос веса
+let weight = parseFloat(prompt("Укажите свой вес:"));
+
+// вычисление разницы между ростом и весом
+let diff = height - weight;
+
+// проверка полученного значения
+if (diff > 110) {
+    alert("Вам нужно набрать вес!");
+
+} else if (diff < 90) {
+    alert("Вам нужно похудеть!");
+
+} else {
+    alert("Ваше вес в норме.");
+}
