182a1662616a8f1b36fc2d6d433b12cdb2cf4d87
Denis Yersov <frontsteron@gmail.com>
2023-03-09 10:47:19 +0500

    A simple calculator in JS

---
+++ Calculator/Calculator.js
@@ -0,0 +1,26 @@
+﻿// функция для сложения
+function add(a, b) {
+   return a + b;
+ }
+ 
+ // функция для вычитания
+ function subtract(a, b) {
+   return a - b;
+ }
+ 
+ // функция для умножения
+ function multiply(a, b) {
+   return a * b;
+ }
+ 
+ // функция для деления
+ function divide(a, b) {
+   if (b === 0) {
+     return "На ноль делить нельзя!"; // обработка ошибки деления на 0
+   }
+   return a / b;
+ }
+ 
+ // пример использования
+ let result = divide(10, 2); // результат будет 5
+ console.log(result);
