eb05602c798a29c06667472db6065dca1ab6de23
Denis Yershov <frontsteron@gmail.com>
2023-10-26 10:31:28 +0500

    fix: litle

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,23 +1,30 @@
-﻿		function add() {
-			let num1 = parseFloat(document.getElementById("num1").value);
-			let num2 = parseFloat(document.getElementById("num2").value);
-			document.getElementById("result").innerHTML = num1 + num2;
-		}
+﻿function add() {
+	let num1 = parseFloat(document.getElementById("num1").value);
+	let num2 = parseFloat(document.getElementById("num2").value);
+	document.getElementById("result").innerHTML = num1 + num2;
+}
 
-		function subtract() {
-			let num1 = parseFloat(document.getElementById("num1").value);
-			let num2 = parseFloat(document.getElementById("num2").value);
-			document.getElementById("result").innerHTML = num1 - num2;
-		}
-
-		function multiply() {
-			let num1 = parseFloat(document.getElementById("num1").value);
-			let num2 = parseFloat(document.getElementById("num2").value);
-			document.getElementById("result").innerHTML = num1 * num2;
-		}
-
-		function divide() {
-			let num1 = parseFloat(document.getElementById("num1").value);
-			let num2 = parseFloat(document.getElementById("num2").value);
-			document.getElementById("result").innerHTML = num1 / num2;
-		}
+function subtract() {
+	let num1 = parseFloat(document.getElementById("num1").value);
+	let num2 = parseFloat(document.getElementById("num2").value);
+	document.getElementById("result").innerHTML = num1 - num2;
+}
+function multiply() {
+	let num1 = parseFloat(document.getElementById("num1").value);
+	let num2 = parseFloat(document.getElementById("num2").value);
+	document.getElementById("result").innerHTML = num1 * num2;
+}
+function divide() {
+	let num1 = parseFloat(document.getElementById("num1").value);
+	let num2 = parseFloat(document.getElementById("num2").value);
+	document.getElementById("result").innerHTML = num1 / num2;
+}
+function divide() {
+	let num1 = parseFloat(document.getElementById("num1").value);
+	let num2 = parseFloat(document.getElementById("num2").value);
+	if (num2 === 0) {
+		 document.getElementById("result").innerHTML = "Нельзя делить на ноль";
+	} else {
+		 document.getElementById("result").innerHTML = num1 / num2;
+	}
+}	  
